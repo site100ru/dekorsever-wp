e66c2382dbcceb99ddc99759d5d9a7a0bcebd56c
Roman Vasilyev <vasilyev-r@mail.ru>
2025-08-27 17:17:53 +0300

    Copyed functions.php from reg.ru

--- functions.php
+++ functions.php
@@ -383,12 +383,11 @@
 	// Подключаем функцию активации мета блока (my_extra_fields)
 	add_action('add_meta_boxes', 'my_extra_fields', 1);
 
-	/*
 	function my_extra_fields() {
 		add_meta_box( 'extra_fields', 'Галерея наших работ', 'extra_fields_box_func', 'portfolio', 'side', 'high' );
-	}*/
+	}
 
-	/* Код блока галереи
+	// Код блока галереи
 	function extra_fields_box_func( $post ){
 		for ($i=1; $i<=9; $i++) { ?>
 			<label>URL&#160;изображения <?php echo $i; ?>:</label>
@@ -397,12 +396,12 @@
 		<? } ?>
 			<input type="hidden" name="extra_fields_nonce" value="<?php echo wp_create_nonce(__FILE__); ?>" />
 		<?php
-	} */
+	} 
 	
 	// включаем обновление полей при сохранении
 	add_action( 'save_post', 'my_extra_fields_update', 0 );
 
-	/* Сохраняем данные, при сохранении поста
+	// Сохраняем данные, при сохранении поста
 	function my_extra_fields_update( $post_id ){
 		// базовая проверка
 		if (
@@ -423,7 +422,7 @@
 			update_post_meta( $post_id, '_'.$key, $value ); // add_post_meta() работает автоматически
 		}
 		return $post_id;
-	} */
+	} 
 	
 	
 	/* Добавляем дополнительные поля к имеющимуся типу записи page */
@@ -809,20 +808,6 @@
 		add_post_type_support('page', 'excerpt');
 	}
 	/*** END ON EXCERPT FOR PAGES ***/
-	
-	
-	
-	
-	/*** ДЕЛАЕМ ПРАВИЛЬНЫЙ TITLE ДЛЯ КАЖДОЙ СТРАНИЦЫ ***/ 
-	function echo_title() {
-		// Если страница портфолио
-		if ( is_post_type_archive( 'portfolio' ) ) {
-			echo 'Наши выполненные работы';
-		} else {
-			echo wp_get_document_title();
-		}
-	}
-	/*** END ДЕЛАЕМ ПРАВИЛЬНЫЙ TITLE ДЛЯ КАЖДОЙ СТРАНИЦЫ ***/
 	
 	
 	
