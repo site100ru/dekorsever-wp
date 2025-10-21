c6fbc91076d788f3e33dcbda049b1c314f4238f1
Slava Sidorov <sidorov-vv3@mail.ru>
2025-10-21 22:30:40 +0500

    тест убрал лишние хлебные крошки

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -32,7 +32,7 @@ get_header( '2' );
  * @hooked woocommerce_breadcrumb - 20
  * @hooked WC_Structured_Data::generate_website_data() - 30
  */
-do_action( 'woocommerce_before_main_content' );
+// do_action( 'woocommerce_before_main_content' );
 
 ?>
 
